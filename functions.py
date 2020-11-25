c60e7911904f5b9d3d7fb167e9e9a5317f827a51
Zealouse <65708398+Zealouse@users.noreply.github.com>
2020-11-25 16:37:04 +0100

    hover

--- functions.py
+++ functions.py
@@ -1,1 +1,6 @@
-
+def hover(a,b):
+    isBetweenX = a[0] >= b[0] and a[0] <= b[0]+b[2]
+    isBetweenY = a[1] >= b[1] and a[1] <= b[1]+b[3]
+    if (isBetweenY and isBetweenX):
+        return True
+    return False
